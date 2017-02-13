cf59ee68c9b8bd43ec1b084babdc6a643c012926
CB <clement.beauvois@primotexto.com>
2017-02-13 17:17:47 +0100

    Adding composer.json

---
+++ src/sarbacane_sdk/Campaigns/SBSmsCampaign.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace sarbacane_sdk\Campaigns;
+
+/**
+ * Description of SBSmsCampaign
+ *
+ * @author guru
+ */
+class SBSmsCampaign {
+    public $id=null;
+    public $name=null;
+    public $type=null;
+    public $sendList=null;
+    public $sourceAddress=null;
+    public $flash=null;
+    public $message=null;
+    public $date=null;
+    public $externalUrl=null;
+    public $landingPageType=null;
+    public $landingPage=null;
+    public $landingPageTitle=null;
+    public $testIdentifier=null;
+}
